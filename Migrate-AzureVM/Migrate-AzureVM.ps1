beabcd297e35cde0f6ff938dcb971417800d25e7
Wei Sun <mct_sunwei@hotmail.com>
2017-03-21 21:28:11 +0800

    Update Migrate-AzureVM.ps1

--- Migrate-AzureVM/Migrate-AzureVM.ps1
+++ Migrate-AzureVM/Migrate-AzureVM.ps1
@@ -1,22 +1,21 @@
-﻿<#
+<#
 .Description
-Migrate Azure IaaS VM from one subscripiton to another subscription
+Move Azure IaaS VM vhd files from one storage to another storage account and keep vm configuration unchanged
 
 .Example
-    .\Migrate-AzureVM.ps1 -SourceSubscriptionName "foo" -DestSubscritpionName "bar" -SourceCloudServiceName "foocs" -SourceVMName "foovm" -DestCloudServiceName "barcs" -DestStorageAccountName "barstorage" -DestLocationName "China East" -DestVNetName "foovnet"
+    .\Move-AzureVHD.ps1 -CloudServiceName "blairvhdmove" -VMName "blairvhdmove" -DestStorageAccountName "blairstorage" -VNetName "RVNET-SH01"
 
+.Program Manager : Qing Liu qliu@microsoft.com
+.Dev: Blair Chen blairch@microsoft.com 
+.Code Reviewer : Qing Liu qliu@microsoft.com; Wei Sun, weisun@microsoft.com 
 #>
 
 Param 
 (
-    [string] $SourceSubscriptionName,
-    [string] $DestSubscritpionName,
-    [string] $SourceCloudServiceName,
-    [string] $SourceVMName,
-    [string] $DestCloudServiceName,
-    [string] $DestStorageAccountName,
-    [string] $DestLocationName,
-    [string] $DestVNetName
+    [string] $CloudServiceName,                   # Cloud service name of VM
+    [string] $VMName,                             # VM Name
+    [string] $DestStorageAccountName,             # Target storage account name
+    [string] $VNetName                            # Virtual network name of VM
 )
 
 #Check the Azure PowerShell module version
@@ -36,16 +35,12 @@ Else
     Exit
 }
 
-Write-Host "`t================= Migration Setting =======================" -ForegroundColor Green
-Write-Host "`t  Source Subscription Name = $SourceSubscriptionName       " -ForegroundColor Green
-Write-Host "`t Source Cloud Service Name = $SourceCloudServiceName       " -ForegroundColor Green
-Write-Host "`t            Source VM Name = $SourceVMName                 " -ForegroundColor Green
-Write-Host "`t    Dest Subscription Name = $DestSubscritpionName         " -ForegroundColor Green
-Write-Host "`t   Dest Cloud Service Name = $DestCloudServiceName         " -ForegroundColor Green
-Write-Host "`t Dest Storage Account Name = $DestStorageAccountName       " -ForegroundColor Green
-Write-Host "`t             Dest Location = $DestLocationName             " -ForegroundColor Green
-Write-Host "`t                 Dest VNET = $DestVNetName                 " -ForegroundColor Green
-Write-Host "`t===========================================================" -ForegroundColor Green
+Write-Host "`t============= Migration Setting ============== " -ForegroundColor Green
+Write-Host "`t Source Cloud Service = $CloudServiceName      " -ForegroundColor Green
+Write-Host "`t Virtual Machine Name = $VMName                " -ForegroundColor Green
+Write-Host "`t Dest Storage Account = $DestStorageAccountName" -ForegroundColor Green
+Write-Host "`t                 VNET = $VNetName              " -ForegroundColor Green
+Write-Host "`t============================================== " -ForegroundColor Green
 
 $ErrorActionPreference = "Stop"
 
@@ -53,17 +48,18 @@ try{ stop-transcript|out-null }
 catch [System.InvalidOperationException] { }
 
 $workingDir = (Get-Location).Path
-$log = $workingDir + "\VM-" + $SourceCloudServiceName + "-" + $SourceVMName + ".log"
+$log = $workingDir + "\VM-" + $CloudServiceName + "-" + $VMName + ".log"
 Start-Transcript -Path $log -Append -Force
 
-Select-AzureSubscription -SubscriptionName $SourceSubscriptionName
+$currentSubscription = Get-AzureSubscription -Current
+$cloudEnv = $currentSubscription.Environment
 
 #######################################################################
 #  Check if the VM is shut down 
 #  Stopping the VM is a required step so that the file system is consistent when you do the copy operation. 
 #  Azure does not support live migration at this time.. 
 #######################################################################
-$sourceVM = Get-AzureVM –ServiceName $SourceCloudServiceName –Name $SourceVMName
+$sourceVM = Get-AzureVM ¨CServiceName $CloudServiceName ¨CName $VMName
 if ( $sourceVM -eq $null )
 {
     Write-Host "[ERROR] - The source VM doesn't exist. Exiting." -ForegroundColor Red
@@ -73,24 +69,24 @@ if ( $sourceVM -eq $null )
 # check if VM is shut down
 if ( $sourceVM.Status -notmatch "Stopped" )
 {
-    Write-Host "[Warning] - Stopping the VM is a required step so that the file system is consistent when you do the copy operation. Azure does not support live migration at this time. If you’d like to create a VM from a generalized image, sys-prep the Virtual Machine before stopping it." -ForegroundColor Yellow
+    Write-Host "[Warning] - Stopping the VM is a required step so that the file system is consistent when you do the copy operation. Azure does not support live migration at this time. If you¡¯d like to create a VM from a generalized image, sys-prep the Virtual Machine before stopping it." -ForegroundColor Yellow
     $ContinueAnswer = Read-Host "`n`tDo you wish to stop $SourceVMName now? (Y/N)"
     If ($ContinueAnswer -ne "Y") { Write-Host "`n Exiting." -ForegroundColor Red; Exit }
     $sourceVM | Stop-AzureVM
 
     # wait until the VM is shut down
-    $sourceVMStatus = (Get-AzureVM –ServiceName $SourceCloudServiceName –Name $SourceVMName).Status
-    while ($sourceVMStatus -notmatch "Stopped") 
+    $VMStatus = (Get-AzureVM ¨CServiceName $CloudServiceName ¨CName $vmName).Status
+    while ($VMStatus -notmatch "Stopped") 
     {
-        Write-Host "Waiting VM $vmName to shut down, current status is $sourceVMStatus" -ForegroundColor Green
+        Write-Host "Waiting VM $vmName to shut down, current status is $VMStatus" -ForegroundColor Green
         Sleep -Seconds 5
-        $sourceVMStatus = (Get-AzureVM –ServiceName $SourceCloudServiceName –Name $SourceVMName).Status
+        $VMStatus = (Get-AzureVM ¨CServiceName $CloudServiceName ¨CName $vmName).Status
     } 
 }
 
 # exporting the sourve vm to a configuration file, you can restore the original VM by importing this config file
 # see more information for Import-AzureVM
-$vmConfigurationPath = $workingDir + "\ExportedVMConfig-" + $SourceCloudServiceName + "-" + $SourceVMName +".xml"
+$vmConfigurationPath = $workingDir + "\ExportedVMConfig-" + $CloudServiceName + "-" + $VMName + ".xml"
 Write-Host "Exporting VM configuration to $vmConfigurationPath" -ForegroundColor Green
 $sourceVM | Export-AzureVM -Path $vmConfigurationPath
 
@@ -106,22 +102,25 @@ $sourceDataDisks = $sourceVM.VM.DataVirtualHardDisks
 
 # Get source storage account information, not considering the data disks and os disks are in different accounts
 $sourceStorageAccountName = $sourceOSDisk.MediaLink.Host -split "\." | select -First 1
-$sourceStorageAccount = Get-AzureStorageAccount –StorageAccountName $sourceStorageAccountName
-$sourceStorageKey = (Get-AzureStorageKey -StorageAccountName $sourceStorageAccountName).Primary 
+$sourceStorageAccount = Get-AzureStorageAccount ¨CStorageAccountName $sourceStorageAccountName
+$sourceStorageKey = (Get-AzureStorageKey -StorageAccountName $sourceStorageAccountName).Primary
+$sourceContext = New-AzureStorageContext ¨CStorageAccountName $sourceStorageAccountName -StorageAccountKey $sourceStorageKey -Environment $cloudEnv
 
-Select-AzureSubscription -SubscriptionName $DestSubscritpionName
 # Create destination context
 $destStorageAccount = Get-AzureStorageAccount | ? {$_.StorageAccountName -eq $DestStorageAccountName} | select -first 1
+$DestStorageAccountName = $destStorageAccount.StorageAccountName
 if ($destStorageAccount -eq $null)
 {
-    New-AzureStorageAccount -StorageAccountName $DestStorageAccountName -Location $DestLocationName
+    if ($sourceStorageAccount.Location -ne $null ) 
+    {
+        New-AzureStorageAccount -StorageAccountName $DestStorageAccountName -Location $sourceStorageAccount.Location
+    } else {
+        New-AzureStorageAccount -StorageAccountName $DestStorageAccountName -AffinityGroup $sourceStorageAccount.AffinityGroup
+    }
     $destStorageAccount = Get-AzureStorageAccount -StorageAccountName $DestStorageAccountName
 }
-$DestStorageAccountName = $destStorageAccount.StorageAccountName
 $destStorageKey = (Get-AzureStorageKey -StorageAccountName $DestStorageAccountName).Primary
-
-$sourceContext = New-AzureStorageContext  –StorageAccountName $sourceStorageAccountName -StorageAccountKey $sourceStorageKey -Environment AzureChinaCloud
-$destContext = New-AzureStorageContext  –StorageAccountName $DestStorageAccountName -StorageAccountKey $destStorageKey
+$destContext = New-AzureStorageContext ¨CStorageAccountName $DestStorageAccountName -StorageAccountKey $destStorageKey -Environment $cloudEnv
 
 # Create a container of vhds if it doesn't exist
 if ((Get-AzureStorageContainer -Context $destContext -Name vhds -ErrorAction SilentlyContinue) -eq $null)
@@ -130,49 +129,82 @@ if ((Get-AzureStorageContainer -Context $destContext -Name vhds -ErrorAction Sil
     New-AzureStorageContainer -Context $destContext -Name vhds
 }
 
+Write-Host "Remove VM $VMName" -ForegroundColor Green
+Remove-AzureVM ¨CServiceName $CloudServiceName ¨CName $VMName
+
+# Copy all vhd disk blobs, including both OS and data disks
 $allDisks = @($sourceOSDisk) + $sourceDataDisks
 $destDataDisks = @()
-# Copy all data disk vhds
-# Start all async copy requests in parallel.
+
+Write-Host "Waiting utill all vhd disk locks are released" -ForegroundColor Green
+do
+{
+    Start-Sleep -Seconds 10
+    $disksInUse = Get-AzureDisk | Where-Object { ($_.AttachedTo.RoleName -eq $VMName) -and ($_.AttachedTo.HostedServiceName -eq $CloudServiceName) }
+    Write-Host "Disk in use: $disksInUse.Count" -ForegroundColor Green
+} while (($disksInUse -ne $null) -or ($disksInUse.Count -gt 0))
+
 foreach($disk in $allDisks)
 {
-    $blobName = $disk.MediaLink.Segments[2]
-    # copy all data disks 
-    Write-Host "Starting copying data disk $($disk.DiskName) at $(get-date)." -ForegroundColor Green
-    $sourceBlob = "https://" + $disk.MediaLink.Host + "/vhds/"
-    $targetBlob = $destStorageAccount.Endpoints[0] + "vhds/"
-    $azcopylog = "azcopy-" + $SourceCloudServiceName + "-" + $SourceVMName +".log"
-    Write-Host "Start copy vhd to destination storage account"  -ForegroundColor Green
-    Write-Host .\Tools\AzCopy.exe /Source:$sourceBlob /Dest:$targetBlob /SourceKey:$sourceStorageKey /DestKey:$destStorageKey /Pattern:$blobName /SyncCopy /v:$azcopylog -ForegroundColor Green
-    .\Tools\AzCopy.exe /Source:$sourceBlob /Dest:$targetBlob /SourceKey:$sourceStorageKey /DestKey:$destStorageKey /Pattern:$blobName /SyncCopy /v:$azcopylog 
+    Write-Host "Remove VM Disk $disk.DiskName" -ForegroundColor Green
+    Remove-AzureDisk -DiskName $disk.DiskName
+}
 
-    if ($disk –eq $sourceOSDisk)
+# Copy all data disk vhds
+foreach($disk in $allDisks)
+{
+    $elapsed = [System.Diagnostics.Stopwatch]::StartNew()
+    $blobName = $disk.MediaLink.Segments[2]
+    Write-Host "Starting copying disk $blobName at $(get-date)" -ForegroundColor Green
+    Write-Host "Source = $sourceContext.BlobEndpoint" -ForegroundColor Green
+    Write-Host "Dest = $destContext.BlobEndpoint" -ForegroundColor Green
+
+    $targetBlob = Start-AzureStorageBlobCopy -SrcContainer vhds -SrcBlob $blobName -DestContainer vhds -DestBlob $blobName -Context $sourceContext -DestContext $destContext -Force
+    do 
     {
-        $destOSDisk = $targetBlob + $blobName
+        if ($copyState.TotalBytes -gt 0 )
+        {
+            $percent = ($copyState.BytesCopied / $copyState.TotalBytes) * 100
+            Write-Host "$blobName copy completed $('{0:N2}' -f $percent)%" -ForegroundColor Green
+        }
+        Start-Sleep -Seconds 10
+        $copyState = $targetBlob | Get-AzureStorageBlobCopyState
+    } while ($copyState.Status -ne "Success")
+    Write-Host "$blobName copy ended at $(get-date)" -ForegroundColor Green
+    Write-Host "$blobName copy total elapsed time: $($elapsed.Elapsed.ToString())" -ForegroundColor Green
+    if ($disk -eq $sourceOSDisk)
+    {
+        $destOSDisk = $targetBlob
     }
     else
     {
-        $destDataDisks += $targetBlob + $blobName
+        $destDataDisks += $targetBlob
     }
 }
 
 # Create OS and data disks 
 Write-Host "Add VM OS Disk " $destOSDisk.MediaLink -ForegroundColor Green
-Add-AzureDisk -OS $sourceOSDisk.OS -DiskName $sourceOSDisk.DiskName -MediaLocation $destOSDisk
-# Attached the copied data disks to the new VM
+Add-AzureDisk -OS $sourceOSDisk.OS -DiskName $sourceOSDisk.DiskName -MediaLocation $destOSDisk.ICloudBlob.Uri
+
 foreach($currenDataDisk in $destDataDisks)
 {
-    $diskName = ($sourceDataDisks | ? {$currenDataDisk.EndsWith($_.MediaLink.Segments[2])}).DiskName
-    Write-Host "Add VM Data Disk $diskName" -ForegroundColor Green
-    Add-AzureDisk -DiskName $diskName -MediaLocation $currenDataDisk
+    $diskName = ($sourceDataDisks | ? {$_.MediaLink.Segments[2] -eq $currenDataDisk.Name}).DiskName
+    Write-Host "Add VM Data Disk $currenDataDisk.ICloudBlob.Uri" -ForegroundColor Green
+    Add-AzureDisk -DiskName $diskName -MediaLocation $currenDataDisk.ICloudBlob.Uri
 }
 
 Write-Host "Import VM from " $vmConfigurationPath -ForegroundColor Green
-Set-AzureSubscription -SubscriptionName $DestSubscritpionName -CurrentStorageAccountName $DestStorageAccountName
+Set-AzureSubscription -SubscriptionName $currentSubscription.SubscriptionName -CurrentStorageAccountName $DestStorageAccountName
 
-# Import VM from previous exported configuration plus vnet info
-if (( Get-AzureService | Where { $_.ServiceName -eq $DestCloudServiceName } ).Count -eq 0 )
+do
 {
-    New-AzureService -ServiceName $DestCloudServiceName -Location $DestLocationName
-}
-Import-AzureVM -Path $vmConfigurationPath | New-AzureVM -ServiceName $DestCloudServiceName -VNetName $DestVNetName -WaitForBoot
+    Write-Host $sourceVM.IpAddress " Waiting till ip becomes available" -ForegroundColor Green
+    Sleep -Seconds 10
+    $ip = Test-AzureStaticVNetIP ¨CVNetName $VNetName ¨CIPAddress $sourceVM.IpAddress
+} while ( $ip.IsAvailable -ne $true )
+
+# Import VM with previous exported configuration plus vnet info
+# TODO: Use Set-AzureStaticVNetIP to make sure private ip is unchanged
+Import-AzureVM -Path $vmConfigurationPath | New-AzureVM -ServiceName $CloudServiceName -VNetName $VNetName -WaitForBoot
+
+Stop-Transcript -ErrorAction SilentlyContinue
